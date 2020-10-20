e80b35a5f87c7fee8fa7324cea3cc69e5446157e
Zeta Gabriels <zetagabriels@gmail.com>
2020-10-19 20:37:55 -0500

    Began C# rewrite.

---
+++ c-sharp/src/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MaddenImporter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ c-sharp/src/models/Player.cs
@@ -0,0 +1,8 @@
+public abstract class Player
+{
+	public string name;
+	public string team;
+	public string position;
+	public int gamesPlayed;
+	public int gamesStarted;
+}
