20ce1927731ebfaa0a379f30e459198267c27053
Zeta Gabriels <zetagabriels@gmail.com>
2020-10-21 21:55:50 -0500

    Stupid idiot baby forgot how generics work

--- c-sharp/src/Extensions.cs
+++ c-sharp/src/Extensions.cs
@@ -1,25 +1,36 @@
 using static System.IO.Directory;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace MaddenImporter
 {
-    public static class Extensions
-    {
-        public static string GenerateUrl(int year, PlayerType playerType)
-        {
-            return $"https://www.pro-football-reference.com/years/{year}/{playerType.ToString().ToLower()}.htm";
-        }
+	public static class Extensions
+	{
+		public static string GenerateUrl(int year, PlayerType playerType)
+		{
+			return $"https://www.pro-football-reference.com/years/{year}/{playerType.ToString().ToLower()}.htm";
+		}
 
-        public static async Task CheckOrCreatePath(string path)
-        {
-            await Task.Run(() =>
-            {
-                if (!Exists(path))
-                {
-                    CreateDirectory(path);
-                    System.Console.WriteLine($"Created directory {path}.");
-                }
-            });
-        }
-    }
+		public static async Task CheckOrCreatePath(string path)
+		{
+			await Task.Run(() =>
+			{
+				if (!Exists(path))
+				{
+					CreateDirectory(path);
+					System.Console.WriteLine($"Created directory {path}.");
+				}
+			});
+		}
+
+		private static string RemapKeys(string dirtyJson)
+		{
+			return string.Empty;
+		}
+
+		public static T ConvertFromJson<T>(string json) where T : Player
+		{
+			return JsonSerializer.Deserialize<T>(json);
+		}
+	}
 }
