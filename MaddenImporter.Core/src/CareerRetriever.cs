7f34266d2efec4774cbbe6f4916d59f39d426f54
Noah Jeleniewski <noah.jeleniewski@snhu.edu>
2020-12-20 18:17:44 -0600

    Edited URL's to accept custom year input

--- MaddenImporter.Core/src/CareerRetriever.cs
+++ MaddenImporter.Core/src/CareerRetriever.cs
@@ -29,11 +29,11 @@ namespace MaddenImporter.Core
             driver = new OpenQA.Selenium.Firefox.FirefoxDriver(geckoPath, firefoxOptions);
         }
 
-        private static string GetCareerUrl(PlayerType playerType, int offset)
+        private static string GetCareerUrl(PlayerType playerType, int offset, int startYear, int endYear)
         {
             urlSuffix.TryGetValue(playerType, out string suffix);
             suffix ??= "";
-            return $"https://stathead.com/football/psl_finder.cgi?request=1&draft_slot_min=1&undrafted=E&draft_year_max={DateTime.Now.Year}&draft_pick_in_round=pick_overall&season_start=1&order_by_asc=0&conference=any&year_min=1995&draft_slot_max=500&match=combined&draft_positions[]=qb&draft_positions[]=rb&draft_positions[]=wr&draft_positions[]=te&draft_positions[]=e&draft_positions[]=t&draft_positions[]=g&draft_positions[]=c&draft_positions[]=ol&draft_positions[]=dt&draft_positions[]=de&draft_positions[]=dl&draft_positions[]=ilb&draft_positions[]=olb&draft_positions[]=lb&draft_positions[]=cb&draft_positions[]=s&draft_positions[]=db&draft_positions[]=k&draft_positions[]=p&year_max={DateTime.Now.Year}&season_end=-1&draft_year_min=1936&draft_type=B&is_active=Y&age_min=0&age_max=99&offset={offset}{suffix}";
+            return $"https://stathead.com/football/psl_finder.cgi?request=1&draft_slot_min=1&undrafted=E&draft_year_max={DateTime.Now.Year}&draft_pick_in_round=pick_overall&season_start=1&order_by_asc=0&conference=any&year_min={startYear}&draft_slot_max=500&match=combined&draft_positions[]=qb&draft_positions[]=rb&draft_positions[]=wr&draft_positions[]=te&draft_positions[]=e&draft_positions[]=t&draft_positions[]=g&draft_positions[]=c&draft_positions[]=ol&draft_positions[]=dt&draft_positions[]=de&draft_positions[]=dl&draft_positions[]=ilb&draft_positions[]=olb&draft_positions[]=lb&draft_positions[]=cb&draft_positions[]=s&draft_positions[]=db&draft_positions[]=k&draft_positions[]=p&year_max={endYear}&season_end=-1&draft_year_min=1936&draft_type=B&age_min=0&age_max=99&offset={offset}{suffix}";
         }
 
         private IEnumerable<string> GetPageAsJson(IEnumerable<IEnumerable<AngleSharp.Dom.IElement>> playerRows, string pos)
@@ -69,19 +69,19 @@ namespace MaddenImporter.Core
                 json = json.Substring(0, json.Length - 1); // remove trailing comma
                 json += "}";
                 jsons.Add(json);
-                Console.Write($"\rParsing... {--playerRowCount} remaining.");
             }
             return jsons;
         }
 
-        private IEnumerable<string> GetPlayersJson(PlayerType playerType)
+        private IEnumerable<string> GetPlayersJson(int startYear, int endYear, PlayerType playerType)
         {
             // pull data
             int offset = 0;
             Extensions.PlayerPositions.TryGetValue(playerType, out string pos);
-            var url = GetCareerUrl(playerType, offset);
+            var url = GetCareerUrl(playerType, offset, startYear, endYear);
             driver.Navigate().GoToUrl(url);
             Console.WriteLine($"Now retrieving {playerType} players.");
+            Console.WriteLine("Please wait... (This may take a while)");
             int playerRowCount = 0;
             List<string> jsons = new List<string>();
             bool paginate = true;
@@ -102,10 +102,7 @@ namespace MaddenImporter.Core
                 if (paginate)
                 {
                     offset += 100;
-                    Console.WriteLine();
-                    Console.WriteLine("Going to next page...");
-                    
-                    url = GetCareerUrl(playerType, offset);
+                    url = GetCareerUrl(playerType, offset, startYear, endYear);
                     driver.Navigate().GoToUrl(url);
                 }
             } while (paginate);
@@ -113,7 +110,7 @@ namespace MaddenImporter.Core
             return jsons;
         }
 
-        public IEnumerable<Player> GetAllPlayers(string username, string password)
+        public IEnumerable<Player> GetAllPlayers(string username, string password, int startYear, int endYear)
         {
             if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
                 throw new ArgumentNullException();
@@ -139,7 +136,7 @@ namespace MaddenImporter.Core
 
             foreach (var enumType in types)
             {
-                var retrieved = GetPlayersJson(enumType);
+                var retrieved = GetPlayersJson(startYear, endYear, enumType);
                 var r = retrieved.Select(p => enumType.ConvertFromJson(p, Extensions.RemapKeys));
                 Console.WriteLine($"Retrieved {retrieved.Count()} {enumType} players.");
                 players = players.Concat(r);
