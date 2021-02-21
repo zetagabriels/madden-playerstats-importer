a14e1b551ca6da9c6d25bbd0cb61364ffb6be7c4
NoahJele <73499563+NoahJele@users.noreply.github.com>
2021-02-20 19:41:00 -0600

    Added custom start and end year parameters. (#7)
    * Test commit
    
    * Edited URL's to accept custom year input
    
    * Added Approximate Value int
    
    * Added Approximate Value int
    
    * Added 'Team' column to Excel sheet output
    
    * Added startYear/endYear options for Career imports
    
    * Updated Executables
    
    * Removed dist/
    
    * Update .gitignore
    
    * Changed parameters in Get and updated error handling.
    
    Co-authored-by: Zeta Gabriels <zetagabriels@gmail.com>

--- MaddenImporter.Excel/src/Program.cs
+++ MaddenImporter.Excel/src/Program.cs
@@ -11,7 +11,7 @@ namespace MaddenImporter.Excel
     {
         static int Main(params string[] args)
         {
-            RootCommand rootCommand = new RootCommand("Retrieves all player info and formats it for Madden 2020.")
+            RootCommand rootCommand = new RootCommand("Retrieves NFL Season/Career Player Stats and formats it for use with the Madden 2020 Franchise Editor.")
             {
                 new Option<int>(
                     new string[]{"--year", "-y"},
@@ -35,31 +35,41 @@ namespace MaddenImporter.Excel
                 new Option<string>(
                     new string[]{"--password", "-p"},
                     description: "For career imports, your stathead.com password."
+                ),
+                new Option<int>(
+                    "--startYear",
+                    getDefaultValue: () => 0, //For example, if endYear is the default 2020, startYear would be 1995
+                    description: "For Career imports, set the year to start at."
+                ),
+                new Option<int>(
+                    "--endYear",
+                    getDefaultValue: () => DateTime.Now.Year,
+                    description: "For Career imports, set the year to end at."
                 )
             };
 
-            rootCommand.Handler = System.CommandLine.Invocation.CommandHandler.Create<int, bool, string, string, string>(ChooseImport);
+            rootCommand.Handler = System.CommandLine.Invocation.CommandHandler.Create<int, bool, string, string, string, int, int>(ChooseImport);
             return rootCommand.InvokeAsync(args).Result;
         }
 
-        static async Task ChooseImport(int year, bool career, string path, string username, string password)
+        static async Task ChooseImport(int year, bool career, string path, string username, string password, int startYear, int endYear)
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("...    Madden Importer v0.1    ...");
+            Console.WriteLine("...    Madden Importer v1.0    ...");
             Console.ForegroundColor = ConsoleColor.White;
             path = System.IO.Path.GetFullPath(path);
             if (!career)
             {
-                Console.WriteLine($"Beginning seasonal import for year {year}.");
+                Console.WriteLine($"Beginning seasonal import for year {year}...");
                 Console.WriteLine($"\tSaving to path {path}");
                 await SeasonalImport(path, year);
             }
 
             if (career)
             {
-                Console.WriteLine("Beginning career import.");
+                Console.WriteLine("Beginning career import...");
                 Console.WriteLine($"\tSaving to path {path}");
-                CareerImport(path, username, password);
+                CareerImport(path, username, password, startYear, endYear);
             }
         }
 
@@ -79,7 +89,7 @@ namespace MaddenImporter.Excel
             workbook.Dispose();
         }
 
-        static void CareerImport(string path, string username, string password)
+        static void CareerImport(string path, string username, string password, int startYear, int endYear)
         {
             if (!System.IO.File.Exists("./geckodriver") && !System.IO.File.Exists("./geckodriver.exe"))
             {
@@ -92,17 +102,27 @@ namespace MaddenImporter.Excel
                 var data = System.IO.File.ReadAllLines("login.private");
                 if (data.Length < 2)
                 {
-                    System.Console.WriteLine("Login file exists, but username and password were not found.");
-                    throw new ArgumentException();
+                    ThrowFatalError("Login file exists, but username and password were not found.");
                 }
                 username ??= data[0].Trim();
                 password ??= data[1].Trim();
             }
 
+            if (startYear > 0 && (startYear < 1950 || startYear > DateTime.Now.Year))
+                ThrowFatalError("Start year is too early.");
+
+            if (endYear < 1950 || endYear > DateTime.Now.Year)
+                ThrowFatalError("End year is invalid.");
+
+            if (startYear == 0)
+                startYear = endYear - 25;
+
+            Console.WriteLine($"Using start year {startYear} and end year {endYear}.");
+
             try
             {
                 using var careerRetriever = new CareerRetriever("./");
-                var players = careerRetriever.GetAllPlayers(username, password).ToList();
+                var players = careerRetriever.GetAllPlayers(username, password, startYear, endYear).ToList();
 
                 var workbook = new ClosedXML.Excel.XLWorkbook();
                 ExcelExtensions.WritePlayerSheet<PassingPlayer>(workbook, Extensions.GetPlayersOfType<PassingPlayer>(players));
@@ -119,12 +139,20 @@ namespace MaddenImporter.Excel
             }
             catch (ArgumentNullException)
             {
-                Console.WriteLine("You must provide a username and password, either through the command-line options, or in a login.private file in the same directory as the executable.");
+                ThrowFatalError("You must provide a username and password, either through the command-line options, or in a login.private file in the same directory as the executable.", "Use the --help parameter if needed.");
             }
             catch (ApplicationException)
             {
-                Console.WriteLine("Login failed. Check your username and password.\nMake sure you can log in with your own browser first.");
+                ThrowFatalError("Login failed. Check your username and password.", "Make sure you can log in with your own browser first.");
             }
+        }
+
+        private static void ThrowFatalError(params string[] errorMessages)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            foreach (var m in errorMessages)
+                Console.WriteLine(m);
+            System.Environment.Exit(1);
         }
     }
 }
